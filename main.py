ff2395a1462c6caa6edf0cef36e21522649452dc
Vza <35027648+voza95@users.noreply.github.com>
2022-03-01 15:35:31 +0530

    Added static data and operations

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,6 +12,10 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_post = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+           {"title": "favorite food", "content": "I like pizza", "id": 2}
+           ]
+
 # async is optional
 @app.get("/")
 async def root():
@@ -19,15 +24,18 @@ async def root():
     return {"message" : "Hello World!!!"}
 
 
-@app.get("/post")
+@app.get("/posts")
 def get_posts():
-    return {"data":"This your post"}
+    return {"data": my_post}
 
-@app.post("/createpost")
+@app.post("/posts")
 def create_post(new_post: Post):
     # Extract all the fields from the body convert it into dictonary and save it in variable name payLoad.
     print(new_post)
     # Convert pydantic to dict
     print(new_post.dict())
-    return {"data":new_post.content}
+    post_dict = new_post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_post.append(post_dict)
+    return {"data": post_dict}
 # title str, content str
